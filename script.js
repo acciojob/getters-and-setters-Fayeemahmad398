772aee9b19e7d946aef5eba834d867555721b0b4
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-07 23:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(`${name} is studing`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+		console.log(`${name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
