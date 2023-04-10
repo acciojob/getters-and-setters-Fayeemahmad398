c9fb6d2f3360b431859340e499837747c88d1025
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-04-11 00:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
-
-
 class Person {
-	constructor(name,age)
-	{
-		this.name=name;
-		this.age=age;
-	}
-	get getters(){
-		return this.name;
-	}
-	set setters(age){
-		this.age=age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
-
-
-
+ 
 class Student extends Person {
-	// constructor(name,age){
-		// super();
-		// this.name=name;
-		// this.age=age;
-		
-	// }
-	function study(){
-		console.log(`${this.name} is studing`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
-
-
+ 
 class Teacher extends Person {
-	// constructor(name,age){
-	// 	super();
-	// 	this.name=name;
-	// 	this.age=age;
-	// }
-	function teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-
-
-// Do not change the code below this line
+ 
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
